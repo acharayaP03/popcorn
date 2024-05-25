870c41119ccf71738ace8049d3d1ec8edd4b7975
acharayaP03 <acharyaP03@gmail.com>
2024-05-25 18:45:57 +1000

    start rating component in use

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import './index.css';
-import App from './App';
-
+// import './index.css';
+// import App from './App';
+import StarRating from "./StarRating";
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
 rootElement.render(
     <React.StrictMode>
-        <App />
+        <StarRating maxRating={5}/>
     </React.StrictMode>
 )
